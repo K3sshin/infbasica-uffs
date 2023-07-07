a0be584267e360c81ee67eae2eb8417dbbbaa085
K3sshin <136598407+K3sshin@users.noreply.github.com>
2023-07-07 09:25:27 -0300

    Update turing.py

--- turing.py
+++ turing.py
@@ -1,1 +1,26 @@
-Ada Lovelace, a primeira mulher a programar.Por Eder Andrade Há quase 200 anos, sob os preconceitos e conceitos do seculo XIX, nascia “a mãe da programação”, Ada Augusta, a condessa de Lovelace, a habilidosa matemática que se tornou a primeira programadora de computadores da nossa história.Que a participação feminina na área de ciências e tecnologia ainda é um pouco tímida, isso é incontestável, mas um fato histórico curioso que pode motivar as estudantes e futuras estudantes de computação é que o primeiro programa de computador da história foi escrito por uma mulher, a “condessa nerd”!Ada Augusta Byron King, a condessa de Lovelace, assumiu esse titulo após se casar aos vinte anos, ficando mais conhecida como Ada Lovelace. Ela era filha de um poeta inglês Lord Byron e Anne Isabelle, conhecida como Annabella. Apesar da fama de seu pai, Annabella não queria que Ada fosse poeta e procurou ensinar a sua filha matemática e música.Ada foi apresentada a Charles Babbage por sua tutora Mary Somerville, Babbage era professor de matemática em Cambridge, conhecido como inventor das maquinas diferenciais (as quais ele nunca terminou), planejava aprimorá-las, com a construção da maquina analítica.Maquina analítica.Ada era uma das poucas pessoas do mundo a entender o funcionamento da maquina programada a cartões perfurados, e em 1843, ela criou um algoritmo para calcular a sequência de Bernoulli, uma dada sequência de números racionais, o algoritmo teria funcionado se a máquina de Babbage tivesse realmente sido construída, mas o projeto somente foi concluído em 2002, pelo Museu da História do Computador, em Londres.Mas o que poucos sabem é que essa não foi a maior contribuição de Ada, historiadores acreditam que sua maior contribuição foi enxergar o computador mecânico além de uma simples calculadora avançada; Ada acreditava que era possível fazer outras operações além de cálculos matemáticos, então, inventou o conceito de sub-rotina, uma sequência de instruções que pode ser usada várias vezes; loop, uma instrução que permite a repetição de uma sequência de cartões perfurados[1]; e do salto condicional, que permite saltar algum cartão caso uma condição seja satisfeita.Na década de 70 o departamento de defesa norte-americano criou uma linguagem de programação chamada Ada, em homenagem a Ada Lovelace, a linguagem originada de pascal era usada em missões criticas, como por exemplo, em softwares de aviação.O Google também homenageou a encantadora dos números, como era chamada carinhosamente por Babbage, criando um doodle, uma personalização da pagina inicial do buscador, em comemoração aos 197 da condessa de Lovelace.
+Ada Lovelace, a primeira mulher a programar.
+Por Eder Andrade Há quase 200 anos, sob os preconceitos e conceitos do seculo XIX, 
+nascia “a mãe da programação”, Ada Augusta, a condessa de Lovelace, 
+a habilidosa matemática que se tornou a primeira programadora de computadores da nossa história.
+Que a participação feminina na área de ciências e tecnologia ainda é um pouco tímida, isso é incontestável, 
+mas um fato histórico curioso que pode motivar as estudantes e futuras estudantes de computação é que o primeiro 
+programa de computador da história foi escrito por uma mulher, a “condessa nerd”!Ada Augusta Byron King, a condessa de Lovelace, 
+assumiu esse titulo após se casar aos vinte anos, ficando mais conhecida como Ada Lovelace. 
+Ela era filha de um poeta inglês Lord Byron e Anne Isabelle, conhecida como Annabella. 
+Apesar da fama de seu pai, Annabella não queria que Ada fosse poeta e procurou ensinar a sua filha matemática e música.
+Ada foi apresentada a Charles Babbage por sua tutora Mary Somerville, Babbage era professor de matemática em Cambridge, 
+conhecido como inventor das maquinas diferenciais (as quais ele nunca terminou), planejava aprimorá-las, 
+com a construção da maquina analítica.Maquina analítica.
+Ada era uma das poucas pessoas do mundo a entender o funcionamento da maquina programada a cartões perfurados,
+e em 1843, ela criou um algoritmo para calcular a sequência de Bernoulli, uma dada sequência de números racionais, 
+o algoritmo teria funcionado se a máquina de Babbage tivesse realmente sido construída, mas o projeto somente foi concluído em 2002, 
+elo Museu da História do Computador, em Londres.Mas o que poucos sabem é que essa não foi a maior contribuição de Ada, 
+historiadores acreditam que sua maior contribuição foi enxergar o computador mecânico além de uma simples calculadora avançada; 
+Ada acreditava que era possível fazer outras operações além de cálculos matemáticos, 
+então, inventou o conceito de sub-rotina, uma sequência de instruções que pode ser usada várias vezes; 
+loop, uma instrução que permite a repetição de uma sequência de cartões perfurados[1]; e do salto condicional, 
+que permite saltar algum cartão caso uma condição seja satisfeita.
+Na década de 70 o departamento de defesa norte-americano criou uma linguagem de programação chamada Ada, em homenagem a Ada Lovelace, 
+a linguagem originada de pascal era usada em missões criticas, como por exemplo, em softwares de aviação.
+O Google também homenageou a encantadora dos números, como era chamada carinhosamente por Babbage, 
+criando um doodle, uma personalização da pagina inicial do buscador, em comemoração aos 197 da condessa de Lovelace.
